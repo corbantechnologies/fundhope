a613a44225b388adcb0242b88e4885d9ca3ca29e
Dalienst Owino <oduordalienst@gmail.com>
2025-09-19 10:14:30 +0300

    fetch camp-aigns

--- services/campaigns.tsx
+++ services/campaigns.tsx
@@ -1,13 +1,13 @@
-import { apiMultipartActions } from "@/tools/api";
+import { apiActions } from "@/tools/api";
 
 export const getCampaigns = async () => {
-  const response = await apiMultipartActions?.get("/api/v1/campaigns/");
+  const response = await apiActions?.get("/api/v1/campaigns/");
 
   return response?.data?.results;
 };
 
 export const getCampaign = async (identity: string) => {
-  const response = await apiMultipartActions?.get(
+  const response = await apiActions?.get(
     `/api/v1/campaigns/${identity}`
   );
 
